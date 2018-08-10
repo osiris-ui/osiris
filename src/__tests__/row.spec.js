4261f64ab83bbfa7407d487a7936be08e51727c4
Matheus Valeriano <math.valeriano@gmail.com>
2018-08-09 21:31:36 -0300

    add snapshot tests

---
+++ src/__tests__/row.spec.js
@@ -0,0 +1,14 @@
+import { shallowMount } from '@vue/test-utils';
+import Row from '../row.vue';
+
+describe('Row', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(Row);
+  });
+
+  it('should match with the snapshot', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
