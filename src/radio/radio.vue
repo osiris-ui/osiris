5c4983db374793cc6e238f0cde18d4ed7cfad286
Matheus Valeriano <math.valeriano@gmail.com>
2018-08-15 20:46:58 -0300

    fix lint radio

--- src/radio/radio.vue
+++ src/radio/radio.vue
@@ -1,74 +1,75 @@
 <template>
   <label
     :class="[
-    `a-radio--${size}`,
-    {'is-disabled': disabled},
-    {'is-checked': isChecked}
+      `a-radio--${size}`,
+      {'is-disabled': disabled},
+      {'is-checked': isChecked}
     ]"
     role="radio"
   >
-  <input
-    type="radio"
-    class="a-radio__inner"
-    aria-hidden="true"
-    :name="name"
-    :value="value"
-    :checked="isChecked"
-    :disabled="disabled"
-    @change.prevent.stop="handleChange"
-  />
-  <span class="a-radio__label">
-    <slot>
-      {{label}}
-    </slot>
-  </span>
-</label>
+    <input
+      :name="name"
+      :value="value"
+      :checked="isChecked"
+      :disabled="disabled"
+      type="radio"
+      class="a-radio__inner"
+      aria-hidden="true"
+      @change.prevent.stop="handleChange"
+    >
+    <span class="a-radio__label">
+      <slot>
+        {{ label }}
+      </slot>
+    </span>
+  </label>
 </template>
 
 <script>
-  export default {
-    model: {
-      prop: 'modelValue',
-      event: 'change'
+export default {
+  name: 'ARadio',
+  model: {
+    prop: 'modelValue',
+    event: 'change',
+  },
+  props: {
+    label: [String, Number],
+    disabled: Boolean,
+    checked: {
+      type: Boolean,
+      default: false,
     },
-    name: 'ARadio',
-    props: {
-      label: [String, Number],
-      disabled: Boolean,
-      checked: {
-        type: Boolean,
-        default: false,
-      },
-      modelValue: {
-        default: undefined,
-      },
-      name: String,
-      value: [Number, String, Boolean],
-      size: {
-        type: String,
-        default: 'medium',
-      },
+    modelValue: {
+      type: [String, Number, Boolean],
+      default: undefined,
     },
-    computed: {
-      isChecked() {
-        if(this.modelValue === undefined) {
-          return this.checked;
-        }
+    name: String,
+    value: [Number, String, Boolean],
+    size: {
+      type: String,
+      default: 'medium',
+    },
+  },
+  computed: {
+    isChecked() {
+      if (this.modelValue === undefined) {
+        return this.checked;
+      }
 
-        return this.modelValue === this.value
+      return this.modelValue === this.value;
+    },
+  },
+  watch: {
+    checked(newValue) {
+      if (newValue !== this.isChecked) {
+        this.handleChange();
       }
     },
-    watch: {
-      checked(newValue) {
-        if (newValue !== this.isChecked) {
-          this.handleChange()
-        }
-      },
+  },
+  methods: {
+    handleChange() {
+      this.$emit('change', this.value);
     },
-    methods: {
-      handleChange() {
-        this.$emit('change', this.value);
-      },
-    },
-  }
+  },
+};
 </script>
