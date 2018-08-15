2126ffb3612deb4e074b539f19d6822abc9832cf
Matheus Valeriano <math.valeriano@gmail.com>
2018-08-14 22:33:59 -0300

    add radio component

---
+++ src/radio/radio.vue
@@ -0,0 +1,72 @@
+<template>
+  <label
+    :class="[
+    `a-radio--${size}`,
+    {'is-disabled': disabled}
+    ]"
+    role="radio"
+  >
+  <input
+    type="radio"
+    class="a-radio__inner"
+    aria-hidden="true"
+    :name="name"
+    :value="value"
+    :checked="isChecked"
+    @change.prevent.stop="handleChange"
+  />
+  <span class="a-radio__label">
+    <slot>
+      {{value}} - {{name}} - {{label}}
+    </slot>
+  </span>
+</label>
+</template>
+
+<script>
+  export default {
+    model: {
+      prop: 'modelValue',
+      event: 'change'
+    },
+    name: 'ARadio',
+    props: {
+      label: [String, Number],
+      disabled: Boolean,
+      checked: {
+        type: Boolean,
+        default: false,
+      },
+      modelValue: {
+        default: undefined,
+      },
+      name: String,
+      value: [Number, String, Boolean],
+      size: {
+        type: String,
+        default: 'medium',
+      },
+    },
+    computed: {
+      isChecked() {
+        if(this.modelValue === undefined) {
+          return this.checked;
+        }
+
+        return this.modelValue === this.value
+      }
+    },
+    watch: {
+      checked(newValue) {
+        if (newValue !== this.isChecked) {
+          this.handleChange()
+        }
+      },
+    },
+    methods: {
+      handleChange() {
+        this.$emit('change', this.value);
+      },
+    },
+  }
+</script>
